7b9e41192366be13586c23ecd891b3804568ae87
Corentin Rivollet <corentin.rivollet@madagence.com>
2025-03-12 22:16:41 +0100

    ADD posts & components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify(),
+  site: "https://astro-crivo.netlify.app/"
 });
