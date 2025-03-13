f70ff0ca3e14356f8ddb58f2773a9277cc3c22bf
Corentin Rivollet <corentin.rivollet@madagence.com>
2025-03-13 09:45:00 +0100

    ADD dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  site: "https://astro-crivo.netlify.app/"
+  site: "https://astro-crivo.netlify.app/",
+  integrations: [preact()]
 });
